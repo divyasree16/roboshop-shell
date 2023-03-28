153a6bc7b2512a0f67a9bd7c8fdc3900cf05c11b
divyasree16 <kdivyasree50@gmail.com>
2023-03-28 03:05:59 +0100

    Keeping code dry

--- mongodb.sh
+++ mongodb.sh
@@ -1,17 +1,17 @@
 source common.sh
 
 print_head "Setup mongodb repository"
-cp ${code_dir}configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>{log_file}
+cp ${code_dir}configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "Intsall mongoDB"
-yum install mongodb-org -y &>>{log_file}
+yum install mongodb-org -y &>>${log_file}
 
 print_head "Update MongoDB Listen Address"
-sed -i -e 's/127.0.0.0/0.0.0.0' /etc/mongod.conf &>>{log_file}
+sed -i -e 's/127.0.0.0/0.0.0.0' /etc/mongod.conf &>>${log_file}
 
 print_head "enable mongoDB"
-systemctl enable mongod &>>{log_file}
+systemctl enable mongod &>>${log_file}
 
 print_head "Start mongoDB service"
-systemctl start mongod &>>{log_file}
+systemctl start mongod &>>${log_file}
 
