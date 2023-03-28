84bab52c624c3a4faecfeec431fc67e7fb09dd9e
divyasree16 <kdivyasree50@gmail.com>
2023-03-28 03:15:26 +0100

    Keeping code dry

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 print_head "Setup mongodb repository"
-cp ${code_dir}configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "Intsall mongoDB"
 yum install mongodb-org -y &>>${log_file}
