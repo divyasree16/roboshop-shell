4fdb6a38fdca2b5d16aa772016141a42c9f9ab8b
divyasree16 <kdivyasree50@gmail.com>
2023-03-28 02:37:40 +0100

    Keeping code dry

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 code_dir=$(pwd)
-log_file=/tmp/roboshop.log
+log_file= /tmp/roboshop.log
 rm -f ${log_file}
 
 print_head() {
--- mongodb.sh
+++ mongodb.sh
@@ -6,10 +6,12 @@ cp ${code_dir}configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>{log_file}
 print_head "Intsall mongoDB"
 yum install mongodb-org -y &>>{log_file}
 
+print_head "Update MongoDB Listen Address"
+sed -i -e 's/127.0.0.0/0.0.0.0' /etc/mongod.conf &>>{log_file}
+
 print_head "enable mongoDB"
 systemctl enable mongod &>>{log_file}
 
 print_head "Start mongoDB service"
 systemctl start mongod &>>{log_file}
 
-#update /etc/mongodb.conf from 127.0.0.1 to 0.0.0.0
