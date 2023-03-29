41e280592f59a981f7d2dd0d076e26430322f023
divyasree16 <kdivyasree50@gmail.com>
2023-03-29 03:07:58 +0100

    Keeping code dry

--- catalogue.sh
+++ catalogue.sh
@@ -2,57 +2,56 @@ source common.sh
 
 print_head "Configure NodeJS Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-status_check() $?
+
 
 print_head "Install NodeJS"
 yum install nodejs -y &>>${log_file}
-status_check() $?
+
 
 print_head "Create Roboshop User"
 useradd roboshop &>>${log_file}
-status_check() $?
+
 
 print_head "Create Application Directory"
 mkdir /app &>>${log_file}
-status_check() $?
+
 
 print_head "Delete old content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
-cd /app
-status_check() $?
+cd /app &>>${log_file}
+
 
 print_head "Extracting App Content"
 unzip /tmp/catalogue.zip &>>${log_file}
-status_check() $?
+
 
 print_head "Installing NoseJS Dependencies"
 npm install &>>${log_file}
-status_check() $?
+
 
 print_head "Copy SystemD Service"
 cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
-status_check() $?
+
 
 print_head "Reload SystemD"
 systemctl daemon-reload &>>${log_file}
-status_check() $?
+
 
 print_head "Enable Catalogue service"
 systemctl enable catalogue &>>${log_file}
-status_check() $?
+
 
 print_head "Start Catalogue service"
 systemctl start catalogue &>>${log_file}
-status_check() $?
+
 
 print_head "Copy mongodb repo file"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
-status_check() $?
+
 
 print_head "Imstall Mongo Client"
 yum install mongodb-org-shell -y &>>${log_file}
-status_check() $?
+
 
 print_head "Load Schema"
 mongo --host mongodb-dev.devops161997.online </app/schema/catalogue.js &>>${log_file}
-status_check() $?
--- frontend.sh
+++ frontend.sh
@@ -10,24 +10,24 @@ status_check() $?
 
 print_head "Downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-status_check() $?
+status_check $?
 
 print_head "Extracting downloaded frontend content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
-status_check() $?
+status_check $?
 
 print_head "Copying nginix config for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-status_check() $?
+status_check $?
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
-status_check() $?
+status_check $?
 
 print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
-status_check() $?
+status_check $?
 
 ##roboshop config is not copied because the servers are not ready
 
