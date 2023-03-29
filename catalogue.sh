0564bf2cebb071ff114140a7bcbb00c063b6f91b
divyasree16 <kdivyasree50@gmail.com>
2023-03-29 02:15:46 +0100

    Keeping code dry

--- catalogue.sh
+++ catalogue.sh
@@ -1,49 +1,45 @@
 source common.sh
 
-print_head "configure NodeJS Repo"
+print_head "Configure NodeJS Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
 print_head "Install NodeJS"
 yum install nodejs -y &>>${log_file}
 
-print_head "Creating Roboshop user"
+print_head "Create Roboshop User"
 useradd roboshop &>>${log_file}
 
-print_head "Create application directory"
+print_head "Create Application Directory"
 mkdir /app &>>${log_file}
 
 print_head "Delete old content"
-rm -rf /app/* &>>${log_file}
-
-print_head "Downloading app content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
 
-print_head "Extracting App content"
+print_head "Extracting App Content"
 unzip /tmp/catalogue.zip &>>${log_file}
-cd /app
 
-print_head "Installing NodeJS Dependencies"
+print_head "Installing NoseJS Dependencies"
 npm install &>>${log_file}
 
-print_head "Copy SystemD Service File"
+print_head "Copy SystemD Service"
 cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
-print_head "Reload systemD"
+print_head "Reload SystemD"
 systemctl daemon-reload &>>${log_file}
 
-print_head "Enable catalogue service"
+print_head" Enable Catalogue service"
 systemctl enable catalogue &>>${log_file}
 
-print_head "Strat Ctalogue Service"
+print_head "Start Catalogue service"
 systemctl start catalogue &>>${log_file}
 
-print_head "Copy MongoDB Repo file"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
-print_head "Install Mongo Client"
+print_head "Copy mongodb repo file"
+cp ${code_dir}/configs/mongodb.rep /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "Imstall Mongo Client"
 yum install mongodb-org-shell -y &>>${log_file}
 
-print_head "Load schema"
+print_head "Load Schema"
 mongo --host mongodb-dev.devops161997.online </app/schema/catalogue.js &>>${log_file}
-
--- frontend.sh
+++ frontend.sh
@@ -9,7 +9,7 @@ rm -rf /usr/share/nginx/html/* &>>${log_file}
 print_head "Downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
- print_head "Extracting downloaded frontend content"
+print_head "Extracting downloaded frontend content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 
@@ -19,7 +19,7 @@ cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
 
- print_head "Starting nginx"
+print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
 
 ##roboshop config is not copied because the servers are not ready
