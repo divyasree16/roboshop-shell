61aea66dff134265da915218fc6a321b3d3d0594
divyasree16 <kdivyasree50@gmail.com>
2023-03-29 03:14:05 +0100

    Keeping code dry

--- frontend.sh
+++ frontend.sh
@@ -2,11 +2,11 @@ source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
-status_check() $?
+status_check $?
 
 print_head "Removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-status_check() $?
+status_check $?
 
 print_head "Downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
