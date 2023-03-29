1ea6b2fd2fd8332e67d3ec33adbbac48992aba23
divyasree16 <kdivyasree50@gmail.com>
2023-03-29 02:59:40 +0100

    Keeping code dry

--- frontend.sh
+++ frontend.sh
@@ -2,25 +2,32 @@ source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
+status_check() $?
 
 print_head "Removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check() $?
 
 print_head "Downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check() $?
 
 print_head "Extracting downloaded frontend content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
+status_check() $?
 
 print_head "Copying nginix config for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check() $?
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
+status_check() $?
 
 print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
+status_check() $?
 
 ##roboshop config is not copied because the servers are not ready
 
--- mongodb.sh
+++ mongodb.sh
@@ -2,16 +2,21 @@ source common.sh
 
 print_head "Setup mongodb repository"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check() $?
 
 print_head "Intsall mongoDB"
 yum install mongodb-org -y &>>${log_file}
+status_check() $?
 
 print_head "Update MongoDB Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>${log_file}
+status_check() $?
 
 print_head "enable mongoDB"
 systemctl enable mongod &>>${log_file}
+status_check() $?
 
 print_head "Start mongoDB service"
 systemctl restart mongod &>>${log_file}
+status_check() $?
 
